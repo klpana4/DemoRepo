985f0b8d830cad31bac7bb1e4fc9e2bcd485169d
Bijjuu <Bijjuu@DESKTOP-D5LTVCT>
2019-04-03 23:41:07 +0530

    newcommit

--- src/main/java/oopsconcept/Bmw.java
+++ src/main/java/oopsconcept/Bmw.java
@@ -5,12 +5,12 @@ public class Bmw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a=0;
-		
-		int c=0;
+int c=0;
 		
 		int b=0;
 		
 		a=b+c;
+		System.out.println("upadted");
 		
 		System.out.println("newchnges");
 	}
