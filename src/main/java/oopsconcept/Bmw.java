e0d0e5d29282ac2547052352b888bfad75f203c0
Bijjuu <Bijjuu@DESKTOP-D5LTVCT>
2019-04-03 23:38:12 +0530

    resolvingconflicts

--- src/main/java/oopsconcept/Bmw.java
+++ src/main/java/oopsconcept/Bmw.java
@@ -4,7 +4,15 @@ public class Bmw {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int a=0;
+		
+		int c=0;
+		
+		int b=0;
+		
+		a=b+c;
+		
+		System.out.println("newchnges");
 	}
 
 }
